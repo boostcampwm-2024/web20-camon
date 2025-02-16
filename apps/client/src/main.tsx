b09504d234aa1fcab5004e08f415f8508ff2ebcc
zero0205 <etety39@naver.com>
2025-02-16 23:46:30 +0900

    :wrench: [Chore]: Tanstack Query 초기 설정

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { RouterProvider } from 'react-router-dom';
-import { router } from '@/app/routes';
+import { App } from './app';
 
-createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
+createRoot(document.getElementById('root')!).render(<App />);
