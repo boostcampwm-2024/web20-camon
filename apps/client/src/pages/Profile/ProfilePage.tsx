414a7b5e6fe6525a830090cb5e58ee6ed78e3244
zero0205 <etety39@naver.com>
2025-02-18 17:41:42 +0900

    :recycle: [Refactor]: Profile 컴포넌트 유저 데이터 페칭 로직 useQuery로 변경

--- apps/client/src/pages/Profile/ProfilePage.tsx
+++ apps/client/src/pages/Profile/ProfilePage.tsx
@@ -1,30 +1,23 @@
 import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { Attendance, UserInfo } from './ui';
 import { UserData } from './model';
 import { EditUserInfo } from '@/features/editProfile';
 import { axiosInstance } from '@/shared/api';
 import { ErrorCharacter, LoadingCharacter } from '@/shared/ui';
 
-export function ProfilePage() {
-  const [userData, setUserData] = useState<UserData | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<Error | null>(null);
-  const [isEditing, setIsEditing] = useState(false);
-  const [showLoading, setShowLoading] = useState(true);
+const getUserInfo = async (): Promise<UserData> => {
+  const response = await axiosInstance.get('/v1/members/info');
+  if (!response.data.success) {
+    throw new Error(response.data.message);
+  }
+  return response.data.data;
+};
 
-  useEffect(() => {
-    axiosInstance
-      .get('/v1/members/info')
-      .then(response => {
-        if (response.data.success) {
-          setUserData(response.data.data);
-        } else {
-          setError(new Error(response.data.message));
-        }
-      })
-      .catch(err => setError(err instanceof Error ? err : new Error(err)))
-      .finally(() => setIsLoading(false));
-  }, [isEditing]);
+export function ProfilePage() {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const { data: userData, isLoading, error } = useQuery({ queryKey: ['userData'], queryFn: getUserInfo });
 
   useEffect(() => {
     if (!userData) return;
@@ -33,19 +26,11 @@ export function ProfilePage() {
     }
   }, [userData, isEditing]);
 
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setShowLoading(false);
-    }, 250);
-
-    return () => clearTimeout(timeoutId);
-  }, []);
-
   const toggleEditing = () => {
     setIsEditing(prev => !prev);
   };
 
-  if (showLoading && isLoading) {
+  if (isLoading) {
     return (
       <div className="flex justify-center items-center">
         <LoadingCharacter size={200} />
@@ -53,7 +38,7 @@ export function ProfilePage() {
     );
   }
 
-  if (error || !userData) {
+  if (error) {
     return (
       <div className="flex justify-center items-center">
         <ErrorCharacter size={200} message={`${'프로필 조회 실패'}`} />
